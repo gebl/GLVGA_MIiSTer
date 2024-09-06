137facd13aa371e3b53b968850269539fc7b4e6a
Gabriel Lawrence <gabriel.lawrence@gmail.com>
2024-09-06 09:22:31 -0500

    VGA timings are now correct. 640x480@60Hz is now working.

--- rtl/GL_VGA.v
+++ rtl/GL_VGA.v
@@ -23,93 +23,70 @@ module GL_VGA
 	output [7:0] vb
 );
 
-parameter hbp = 8;        // end of horizontal back porch
-parameter hfp = 632;        // beginning of horizontal front porch
-parameter vbp = 8;         // end of vertical back porch
-parameter vfp = 472;        // beginning of vertical front porch
-
 /*
-parameter hbp = (TOTAL_COLS-ACTIVE_COLS)/2;        // end of horizontal back porch
-parameter hfp = ACTIVE_COLS+hbp;        // beginning of horizontal front porch
-parameter vbp = (TOTAL_ROWS-ACTIVE_ROWS)/2;         // end of vertical back porch
-parameter vfp = ACTIVE_ROWS+vbp;        // beginning of vertical front porch
+// http://www.tinyvga.com/vga-timing/640x480@60Hz
+
+General timing
+Screen refresh rate	60 Hz
+Vertical refresh	31.46875 kHz
+Pixel freq.	25.175 MHz
+
+Horizontal timing (line)
+Polarity of horizontal sync pulse is negative.
+
+Visible area	640	25.422045680238
+Front porch	16	0.63555114200596
+Sync pulse	96	3.8133068520357
+Back porch	48	1.9066534260179
+Whole line	800	31.777557100298
+
+Vertical timing (frame)
+Polarity of vertical sync pulse is negative.
+
+Visible area	480	15.253227408143
+Front porch	10	0.31777557100298
+Sync pulse	2	0.063555114200596
+Back porch	33	1.0486593843098
+Whole frame	525	16.683217477656
+
+Components of VGA signal 
+http://javiervalcarce.eu/html/vga-signal-format-timming-specs-en.html
+
+HSYNC  Horizontal sync. Make electron beam restart at next screen's scanline (starts a new line).
+VSYNC  Vertical sync. Make electron beam restart at first screen's scanline (starts a new frame).
+R      Red intensity. 
+G      Green intensity.
+B      Blue intensity.
+
+Pulses on HSYNC signal mark the start and end of a line and ensure that the monitor displays the pixels between the left and right edges of the visible screen area.
+The active region is followed by a blanking region, in which black pixels are transmitted. In the middle of the blanking interval, a horizontal sync pulse is transmitted. The blanking interval before the sync pulse is known as the "front porch", and the blanking interval after the sync pulse is known as the "back porch".
+
+Pulses on VSYNC signal mark the start and end of a frame made up of video lines and ensure that the monitor displays the lines between the top and bottom edges of the visible monitor screen.
+
+
+The horizontal resolution of each line '''is not actually determined''' and could be anything, this resolution is determined typically by a '''pixel clock'''. Every rising edge of the pixel clock marks the start of a new pixel.
 */
+
+localparam hviz = 640;
+localparam hfp = 16;
+localparam hsp = 96;
+localparam hbp = 48;
+localparam htotal = 800;
+
+localparam vviz = 480;
+localparam vfp = 10;
+localparam vsp = 2;
+localparam vbp = 33;
+localparam vtotal = 525;
 
 reg   [10:0] hc;
 reg   [10:0] vc;
 
-wire h_active;
-wire v_active;
-
-
-assign h_active = (hc >= hbp && hc < hfp);
-assign v_active = (vc >= vbp && vc < vfp);
-
-/*
 always @(posedge clk) begin
-	ce_pix <= ~ce_pix;
-	if(reset) begin
-		ce_pix <= 0;
-		hc <= 0;
-		vc <= 0;
-		hedge <= 0;
-		vedge <= 0;
-		hgrid<=0;
-		vgrid<=0;
-	end
-	else if(ce_pix) begin
-		if(hc == TOTAL_COLS-1) begin
-			hc <= 0;
-			if(vc == TOTAL_ROWS-1) begin 
-				vc <= 0;
-			end else begin
-				vc <= vc + 1'd1;
-			end
-		end else begin
-			hc <= hc + 1'd1;
-		end
-	end
-	if(h_active) begin
-		HBlank <= 0;
-		HSync <= 1;
-	end else begin
-		HSync <= 0;
-		HBlank <= 1;
-	end
-
-	if (v_active) begin
-		VBlank <= 0;
-		VSync <= 1;
-	end else begin
-		VSync <= 0;
-		VBlank <= 1;
-	end
-
-	if (h_active) begin
-		hedge <= hedge + 1'd1;
-		if(hedge == 7) begin
-			hgrid <= hgrid + 1'd1;
-		end
-	end else begin
-		hedge <= 0;
-		hgrid <= 0;
-	end
-
-	if (v_active) begin
-		vedge <= vedge + 1'd1;
-		if(vedge == 7) begin
-			vgrid <= vgrid + 1'd1;
-		end
-	end else begin
-		vedge <= 0;
-		vgrid <= 0;
-	end
-end
-*/
-always @(posedge clk) begin
-	if(scandouble) ce_pix <= 1;
+	/*if(scandouble) ce_pix <= 1;
 		else ce_pix <= ~ce_pix;
-
+	*/
+	ce_pix<=1;
 
 	if(reset) begin
 		hc <= 0;
@@ -129,86 +106,71 @@ always @(posedge clk) begin
 			hc <= hc + 1'd1;
 		end
 	end
+	
 	if(hc < ACTIVE_COLS) begin
 		HBlank <= 0;
-		HSync <= 1;
+		//HSync <= 1;
 	end else begin
-		HSync <= 0;
+		//HSync <= 0;
 		HBlank <= 1;
 	end
 
 	if (vc < ACTIVE_ROWS) begin
 		VBlank <= 0;
-		VSync <= 1;
+		//VSync <= 1;
 	end else begin
-		VSync <= 0;
+		//VSync <= 0;
 		VBlank <= 1;
 	end
 end
 
+//assign HBlank = (hc < hviz) ? 0 : 1;
+//assign VBlank = (vc < vviz) ? 0 : 1;
 
-/*
-always @(posedge clk) begin
-	if (hc == 529) HBlank <= 1;
-		else if (hc == 0) HBlank <= 0;
-
-	if (hc == 544) begin
-		HSync <= 1;
-
-		if(vc == 490 ) VSync <= 1;
-			else if (vc == (scandouble ? 496 : 248)) VSync <= 0;
-
-		if(vc == 480) VBlank <= 1;
-			else if (vc == 0) VBlank <= 0;
-	end
-	
-	if (hc == 590) HSync <= 0;
-end
-*/
-/*
-always @(posedge clk) begin
-	if (h_active && v_active) begin
-		video <= 8'b00000000;
-		vr <=  (hgrid==79 || hgrid==0 || vgrid==0 || vgrid==24) ? 8'b11111111:8'b00000000;
-		vg <= (vedge==7) ? 8'b11111111:8'b00000000;
-		vb <= (hedge==7 ) ? 8'b11111111 : 8'b00000000;
-	end else begin
-		video <= 8'b11111111;
-		vr <= 8'b11111111;
-		vg <= 8'b11111111;
-		vb <= 8'b11111111;
-	end
-end
-*/
+assign HSync = (hc >= hviz+hbp && hc<hviz+hbp+hsp) ? 1 : 0;
+assign VSync = (vc >= vviz+vbp && vc<vviz+vbp+vsp) ? 1 : 0;
 
 always @(posedge clk) begin
 		vr <=  8'b00000000;
 		vg <= 8'b00000000;
 		vb <= 8'b00000000;
 
+		if (hc[3:3]==1) begin
+			vr <= 8'b11111111;
+		end else begin
+			vr <= 8'b00000000;
+		end
+
+		if (vc[3:3]==1) begin
+			vb <= 8'b00000000;
+		end else begin
+			vb <= 8'b11111111;
+		end
+
 		//vg <= (vc[3:0]==0) ? 8'b11111111:8'b00000000;
 		//vb <= (hc[3:0]==0 ) ? 8'b11111111 : 8'b00000000;
-		if (vc[10:3]==1) begin
-			if (hc[3:3]==1) begin
-				vr <= 8'b11111111;
-				vg <= 8'b11111111;
-				vb <= 8'b11111111;
-			end else begin
-				vr <= 8'b00000000;
-				vg <= 8'b11111111;
-				vb <= 8'b11111111;
-			end
-		end
-		if (vc[10:3]==3) begin
+
+		if (vc[10:3]==0 || vc[10:3]>=59) begin
 			vr <= 8'b11111111;
 			vg <= 8'b11111111;
 			vb <= 8'b11111111;
 		end
-		if (vc[10:3]==2) begin
-			vr <= 8'b00000000;
-			vg <= 8'b00000000;
+		if (hc[10:3]==0 || hc[10:3]>=79) begin
+			vr <= 8'b11111111;
+			vg <= 8'b11111111;
 			vb <= 8'b11111111;
 		end
-		
+
+		if (hc==32 || hc==40) begin
+			vr <= 8'b11111111;
+			vg <= 8'b11111111;
+			vb <= 8'b11111111;
+		end
+
+		if (vc==32 || vc==40) begin
+			vr <= 8'b11111111;
+			vg <= 8'b11111111;
+			vb <= 8'b11111111;
+		end
 end
 endmodule
